7cf35c7dc30fe90b5abc7b4402f99dbd547e1d38
Farvd <davidadelaide777@gmail.com>
2026-08-18 14:37:10 +0930

    Added Player Shooting
    -Added Hit scan shooting
    -Added temp hit locater
    -Added visual trails for the bullets

---
+++ Survival Shooter Game/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,54 @@
+using Mono.Cecil;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject LocatorPrefab;
+    public TrailRenderer BulletTrailPrefab;
+    public GameObject HitLocater;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Mouse.current.leftButton.isPressed))
+        {
+            Shoot();
+        }
+        
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100f))
+        {
+            GameObject locator = Instantiate(LocatorPrefab, hit.point, Quaternion.identity);
+            Destroy(locator, 10f);
+            TrailRenderer trail = Instantiate(BulletTrailPrefab, Camera.main.transform.position, Quaternion.identity);
+            StartCoroutine(spawnTrail(trail, hit.point));
+        }
+    }
+
+    private IEnumerator spawnTrail(TrailRenderer trail, Vector3 hitPoint)
+    {
+        float time = 0;
+        Vector3 startPosition = trail.transform.position;
+        while (time < 1)
+        {
+            trail.transform.position = Vector3.Lerp(startPosition, hitPoint, time);
+            time += Time.deltaTime / trail.time;
+            yield return null;
+        }
+        trail.transform.position = hitPoint;
+        Destroy(trail.gameObject, trail.time);
+    }
+}
+
