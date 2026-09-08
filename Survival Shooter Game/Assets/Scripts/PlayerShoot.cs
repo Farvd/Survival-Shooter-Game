874ce2c477e521934000f896b30c370067402bd0
Farvd <davidadelaide777@gmail.com>
2026-09-08 12:36:13 +0930

    made build
    still very buggy

--- Survival Shooter Game/Assets/Scripts/PlayerShoot.cs
+++ Survival Shooter Game/Assets/Scripts/PlayerShoot.cs
@@ -1,7 +1,7 @@
-using Mono.Cecil;
+//using Mono.Cecil;
 using System.Collections;
 using System.Runtime.CompilerServices;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
